a89660c176986dc701e57e84fd3c254fc2c3ee6b
ertugrulerol <ertugrulerol@localhost>
2022-09-25 19:54:32 +0300

    Second Commit

--- classesWithAttributes/src/classesWithAttributes/Product.java
+++ classesWithAttributes/src/classesWithAttributes/Product.java
@@ -1,13 +1,68 @@
 package classesWithAttributes;
 
 public class Product {
+
+	public Product () {
+		System.out.println("Yapıcı Blok Çalıştı.");
+	}
 	
-	//attribute veya field 
-	int id;
-	String name;
-	String description;
-	double price;
-	int stockAmount;
+	// attribute veya field
+
+	private int id;
+	private String name;
+	private String description;
+	private double price;
+	private int stockAmount;
+	
+
+	// getter
+	public int getId() {
+		// get yapılırken okunan veri değiştirilebilir. Başka işlemler de yapılabilir.
+		return id;
+		// setter
+	}
+
+	public void setId(int id) {
+		this.id = id;
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getStockAmount() {
+		return stockAmount;
+	}
+
+	public void setStockAmount(int stockAmount) {
+		this.stockAmount = stockAmount;
+	}
+
+	public String getKod() {
+		return this.name.substring(0,1) + id;
+	}
+
 	
 
 }
