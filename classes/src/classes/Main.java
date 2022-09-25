9312a2cab890565d4235dc384ba7787228a43027
ertugrulerol <ertugrulerol@localhost>
2022-09-25 18:21:32 +0300

    First Commit

---
+++ classes/src/classes/Main.java
@@ -0,0 +1,35 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//reference type
+		
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+		
+	//value Değer Tip
+		int sayi1 =10;
+		int sayi2 =20;
+		sayi2 = sayi1;
+		sayi1 =30;
+		System.out.println(sayi2);
+
+	// reference Referans Tipte önemli olan değer değil verilen adres bilgisidir.
+	// referans tanımlaması yapıldığından sonradan atanan değerler önemli değildir.
+		
+		int [] sayilar1 = new int[] {1,2,3};
+		int [] sayilar2 = new int[] {4,5,6};
+		sayilar2 = sayilar1;
+		sayilar1[0]=10;
+		System.out.println(sayilar2[0]);
+		
+		
+	}
+
+}
+
+
