bc8c7238c4e299c5afeb98c5aac3628e66b1e3a4
ertugrulerol <ertugrulerol@localhost>
2022-09-25 18:29:32 +0300

    First Commit

---
+++ recapDemo_classes/src/recapDemo_classes/Main.java
@@ -0,0 +1,13 @@
+package recapDemo_classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		
+		int sonuc = dortIslem.Topla(5, 4);
+		System.out.println(sonuc);
+
+	}
+
+}
