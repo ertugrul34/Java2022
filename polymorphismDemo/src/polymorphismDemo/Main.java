8f3631272796ecadbf10499e0950b6c791f547fd
ertugrulerol <ertugrulerol@localhost>
2022-09-27 00:35:48 +0300

    First Commit

---
+++ polymorphismDemo/src/polymorphismDemo/Main.java
@@ -0,0 +1,19 @@
+package polymorphismDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		BaseLogger[] loggers = new BaseLogger[] { new FileLogger(), new EmailLogger(), new DatabaseLogger(), new ConsoleLogger()};
+//		
+//		for (BaseLogger logger:loggers) {
+//			logger.log("Log mesajı");
+//		}
+
+		// Polimorfizm örneği
+
+		CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+		customerManager.Add();
+
+	}
+
+}
