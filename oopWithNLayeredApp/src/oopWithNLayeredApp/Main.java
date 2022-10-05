b67db558329b1ebd379eb19ea2c4a74856e57ae3
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-10-05 23:27:57 +0300

    Second Commit

--- oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/Main.java
@@ -1,6 +1,10 @@
 package oopWithNLayeredApp;
 
 import oopWithNLayeredApp.business.ProductManager;
+import oopWithNLayeredApp.core.logging.DatabaseLogger;
+import oopWithNLayeredApp.core.logging.FileLogger;
+import oopWithNLayeredApp.core.logging.Logger;
+import oopWithNLayeredApp.core.logging.MailLogger;
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.entities.Product;
@@ -11,7 +15,9 @@ public class Main {
 
 		Product product1 = new Product(1, "Iphone XR", 10000);
 
-		ProductManager productManager = new ProductManager(new HibernateProductDao());
+		Logger[] loggers = {new DatabaseLogger(), new FileLogger(), new MailLogger()};
+		
+		ProductManager productManager = new ProductManager(new HibernateProductDao(), loggers);
 		productManager.add(product1);
 
 	}
--- oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
+++ oopWithNLayeredApp/src/oopWithNLayeredApp/business/ProductManager.java
@@ -1,5 +1,6 @@
 package oopWithNLayeredApp.business;
 
+import oopWithNLayeredApp.core.logging.Logger;
 import oopWithNLayeredApp.dataAccess.HibernateProductDao;
 import oopWithNLayeredApp.dataAccess.JdbcProductDao;
 import oopWithNLayeredApp.dataAccess.ProductDao;
@@ -7,9 +8,11 @@ import oopWithNLayeredApp.entities.Product;
 
 public class ProductManager {
 	private ProductDao productDao;
+	private Logger[] loggers;
 
-	public ProductManager(ProductDao productDao) {
+	public ProductManager(ProductDao productDao, Logger[] loggers) {
 		this.productDao = productDao;
+		this.loggers = loggers;
 	}
 
 	public void add(Product product) throws Exception {
@@ -18,6 +21,10 @@ public class ProductManager {
 			throw new Exception("Ürün Fiyatı 10 dan küçük olamaz");
 		}
 		productDao.add(product);
+		
+		for (Logger logger : loggers) {
+			logger.log(product.getName());
+		}
 
 	}
 
