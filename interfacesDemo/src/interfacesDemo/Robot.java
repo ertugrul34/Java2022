06d4bd8d7ef712b0402469e25d912476e426cff4
Ertuğrul EROL <73794808+ertugrul34@users.noreply.github.com>
2022-10-11 21:41:02 +0300

    First Commit

---
+++ interfacesDemo/src/interfacesDemo/Robot.java
@@ -0,0 +1,17 @@
+package interfacesDemo;
+
+public class Robot implements IWorkable,IMaintable{
+
+	@Override
+	public void work() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void maintain() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
